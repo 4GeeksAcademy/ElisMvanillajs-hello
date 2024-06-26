63d8b20a70f4b289b727808877d39824d8a495db
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-06-26 18:54:34 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let Who = ["El carro", "Mi hija", "El caballo", "El abuelo"];
+let Action = ["corre", "toca", "come", "duerme"];
+let What = ["mi comida", "mi guitarra", "el celular", "el vestido"];
+let When = [
+  "despues de dormir",
+  "en la tarde",
+  "casi nunca",
+  "todas las noches"
+];
+
+let indiceWho = Who[Math.floor(Math.random() * Who.length)];
+let indiceAction = Action[Math.floor(Math.random() * Action.length)];
+let indiceWhat = What[Math.floor(Math.random() * What.length)];
+let indiceWhen = When[Math.floor(Math.random() * When.length)];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excusa.innerText =
+    indiceWho + " " + indiceAction + " " + indiceWhat + " " + indiceWhen;
 };
